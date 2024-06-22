e0d6f0b7913e375d78f9471f0422eee49a20c3a8
Rodrigo Pauletti <digopauletti@yahoo.com.br>
2024-06-22 14:28:05 -0300

    ⚡️ Add anchor links between login/register/forgot password form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,16 +32,28 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button>
                 {{ __('Log in') }}
             </x-primary-button>
+        </div>
+
+        <div class="flex items-center justify-start mt-4 text-sm text-gray-600">
+            @if (Route::has('register'))
+                <p>{{ __('Not registered yet?') }}</p>
+                <a
+                    href="{{ route('register') }}"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-1"
+                >
+                    {{ __('Create account') }}
+                </a>
+            @endif
         </div>
     </form>
 </x-guest-layout>
